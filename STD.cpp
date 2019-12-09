0b51fb59ff5cbf12b0c53fd106e1a8a6e362115c
Eman <eman1371994@gmail.com>
2019-12-09 13:23:16 +0200

    update SDT Double file

--- STD.cpp
+++ STD.cpp
@@ -1,4 +1,7 @@
 #include "header.h"
+#include "math.h"
+
+
 
 
 int Function::STD(vector<int>Vector)
@@ -9,8 +12,22 @@ int Function::STD(vector<int>Vector)
 	{
 		sum += Vector[i];
 	}
-	mean = sum /Vector.size();
+	mean = sum / Vector.size();
 	for (i = 0; i < Vector.size(); ++i)
 		standardDeviation += pow(Vector[i] - mean, 2);
-	return sqrt(standardDeviation /Vector.size());
+	return int(sqrt(standardDeviation / Vector.size()));
+}
+
+double Function::STD(vector<double>Vector)
+{
+	double sum = 0, mean, standardDeviation = 0;
+	int i;
+	for (i = 0; i < Vector.size(); ++i)
+	{
+		sum += Vector[i];
+	}
+	mean = sum / Vector.size();
+	for (i = 0; i < Vector.size(); ++i)
+		standardDeviation += pow(Vector[i] - mean, 2);
+	return sqrt(standardDeviation / Vector.size());
 }
